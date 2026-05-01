66d56dd7d0c7bc66e2ea0f6e3cdde2e68066dd07
“prakashsauav123@gmail.com” <prakashsauav123@gmail.com>
2026-05-01 17:19:46 +0530

    added top headline viewmodel

--- app/src/main/java/com/saurav/newsapp_mvvm_architecture/ui/theme/topHeadlines/TopHeadlinesViewModel.kt
+++ app/src/main/java/com/saurav/newsapp_mvvm_architecture/ui/theme/topHeadlines/TopHeadlinesViewModel.kt
@@ -1,4 +1,45 @@
 package com.saurav.newsapp_mvvm_architecture.ui.theme.topHeadlines
 
-class TopHeadlinesViewModel {
+import android.provider.ContactsContract.CommonDataKinds.StructuredPostal.COUNTRY
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.saurav.newsapp_mvvm_architecture.data.model.Article
+import com.saurav.newsapp_mvvm_architecture.data.model.Country
+import com.saurav.newsapp_mvvm_architecture.data.repository.TopHeadLineRepository
+import com.saurav.newsapp_mvvm_architecture.di.module.BackgroundContext
+import com.saurav.newsapp_mvvm_architecture.ui.theme.base.UiState
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+class TopHeadlinesViewModel @Inject constructor(
+    private val repository: TopHeadLineRepository,
+    @BackgroundContext private val dispatcher: CoroutineDispatcher
+) : ViewModel() {
+
+    private val _uiState = MutableStateFlow<UiState<List<Article>>>(
+        UiState.Initial
+    )
+    private val uiState: StateFlow<UiState<List<Article>>> = _uiState
+
+    fun loadTopHeadlines(country: String = "us") {
+        viewModelScope.launch {
+            _uiState.value = UiState.Loading
+            repository.getTopHeadLine(country).onStart {
+                _uiState.value = UiState.Loading
+            }.catch {
+                _uiState.value = UiState.Error(it.message)
+            }.flowOn(dispatcher).collect { articles ->
+                _uiState.value = UiState.Success(articles)
+            }
+        }
+
+    }
+
+
 }
